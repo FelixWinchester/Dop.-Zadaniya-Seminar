3587bdc57ff605f8fbd761b53128082cdf841117
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-01-17 21:55:19 +0400

    Update DOP DZ - 3.2 .cpp

--- DOP DZ - 3.2 .cpp
+++ DOP DZ - 3.2 .cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n, x, s = 0;
+	cin >> n >> x;
+	for (int i = 0; i <= n; i++) {
+		s += pow(x, i) * (i % 2 == 0 ? 1 : -1);
+	}
+	cout << s;
+}
