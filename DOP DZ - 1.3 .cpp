7a1971f90c9977ef33037e2d08ad9f140ab2834e
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-01-17 21:41:56 +0400

    Create DOP DZ - 1.3 .cpp

---
+++ DOP DZ - 1.3 .cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    switch (a) {
+    case 1:
+        cout << "Do"; break;
+    case 2:
+        cout << "Re"; break;
+    case 3:
+        cout << "Mi"; break;
+    case 4:
+        cout << "Fa"; break;
+    case 5:
+        cout << "Sol'"; break;
+    case 6:
+        cout << "Lya"; break;
+    case 7:
+        cout << "Si"; break;
+    }
+    return 0;
+}
