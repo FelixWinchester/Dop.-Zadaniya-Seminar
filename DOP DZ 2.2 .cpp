c40c06670f47602644fedebe6578b57bdb456020
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-01-17 21:47:50 +0400

    Create DOP DZ 2.2 .cpp

---
+++ DOP DZ 2.2 .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    while (n - x > 0) {
+        cout << n - x << endl;
+        n -= x;
+    }
+}
